5e4b2a9562fcc70d8c81dd14208796c9aae59dc8
lanhao <admin@lanhao.name>
2021-05-11 11:04:04 +0800

    feat: 准备检测node环境

--- src/dev.action.ts
+++ src/dev.action.ts
@@ -1,5 +1,6 @@
-import { ActionBase, ArgvOption, IWithSubCommands } from '@mohism/sloty';
+import { ActionBase, ArgvOption } from '@mohism/sloty';
 import { Dict } from '@mohism/utils';
+import { existsSync } from 'fs';
 
 class DevAction extends ActionBase {
   options(): Dict<ArgvOption> {
@@ -10,11 +11,28 @@ class DevAction extends ActionBase {
     return '以开发模式运行当前目录，以 npm start 为准';
   }
 
+  loadPkg() {
+    const pkgPath = `${this.instance.root}/package.json`;
+    if (!existsSync(pkgPath)) {
+      this.fatal(`NOT FOUND: ${pkgPath}`);
+    }
+    return require(pkgPath);
+  }
 
+  detectNodeVersion(pkg: { engines?: { node?: string } }) {
+    if (pkg.engines?.node) {
+      const rules = pkg.engines?.node.split(' ');
+      // how to find the correct version ?
+      console.log(rules);
+    }
+    return 'latest';
+  }
 
-  async run(options: IWithSubCommands): Promise<void> {
-    // start here
-    console.log(options);
+  async run(): Promise<void> {
+    // load pkg
+    const pkg = this.loadPkg();
+
+    console.log(pkg);
     this.info('hello');
   }
 }
